c49630dc795e7b23fb2ff743f3cd28ee6c6f0ee6
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-06-27 15:07:23 -0300

    Adicionando os primeiros arquivos do projeto

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-// index.js
+const fastify = require('fastify')({ logger: true });
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'postgres',
+  user: 'user',
+  password: 'password',
+  database: 'meu_banco'
+});
+
+fastify.get('/', async (request, reply) => {
+  const result = await pool.query('SELECT NOW()');
+  return {
+    instancia: process.env.HOSTNAME,
+    horario_db: result.rows[0].now
+  };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
